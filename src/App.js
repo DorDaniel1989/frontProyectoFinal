c1011d70fd54b3f48ddf71ba49c27aea072ae117
DorDaniel1989 <danielovelleiro@outlook.es>
2022-03-28 12:56:34 +0200

    Trayendo imagenes de bbdd

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {useRef} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, {Component, useEffect, useState } from "react";
 import Menu from './Menu';
+import Carrusel from './Carrusel';
+import Carta from './Carta';
 
 import './miCss.css';
 import { render } from "@testing-library/react";
@@ -12,11 +14,23 @@ function App() {
 
 
   return (
-    <div >
-      <h1>EVENTOS</h1>
-      <img src={imagen4}/>
-      <Menu/>
-
+    <div>
+      <div className="cabecera"> <h1>EVENTOS</h1>
+     <img src={imagen4}/></div>
+    <Menu/>
+    <Carrusel/>
+    <Carta/>
+  
+    {/* <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="registro" element={<Formulario/>} />
+        <Route path="contacto" element={<Login/>} />
+        <Route path="login" element={<Register/>} />
+       
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter> */}
     </div>
   );}
 
--- src/Menu.js
+++ src/Menu.js
@@ -7,16 +7,14 @@ import React, { useEffect, useState } from "react";
 import './miCss.css';
 
 function Menu() {
-    
+
   return (
-    <div className="navbar" style={{
-         display :"flex"
-         }}>
-     <div><a href="/">Home</a></div>
-     <div><a href="/">Eventos</a></div>
-     <div><a href="/">Registrarse</a></div>
-     <div><a href="/">Loguear</a></div>
-     <div> <a href="/">Contacto</a></div>
+    <div className="navbar" style={{ display :"flex"}}>
+     <div  className = "opcionMenu"><a href="/">Home</a></div>
+     <div  className = "opcionMenu"><a href="/">Eventos</a></div>
+     <div  className = "opcionMenu"><a href="/">Registrarse</a></div>
+     <div  className = "opcionMenu"><a href="/">Loguear</a></div>
+     <div  className = "opcionMenu"><a href="/">Contacto</a></div>
     </div>
   );
 }
