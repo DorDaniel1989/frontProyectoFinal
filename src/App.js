7a5d32167a6c96fc338ea39e9805a53be1e409c3
DorDaniel1989 <danielovelleiro@outlook.es>
2022-03-28 09:23:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import imagen4 from "./concierto.png";
+import {useRef} from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, {Component, useEffect, useState } from "react";
+import Menu from './Menu';
+
+import './miCss.css';
+import { render } from "@testing-library/react";
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>EVENTOS</h1>
+      <img src={imagen4}/>
+      <Menu/>
+
     </div>
-  );
-}
+  );}
+
 
 export default App;
