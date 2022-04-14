5be69014af27af04f3fc3dabc42e40dc293e01bb
DorDaniel1989 <danielovelleiro@outlook.es>
2022-04-14 10:59:59 +0200

    detalle eventos + comentarios de evento

--- src/components/Carta.js
+++ src/components/Carta.js
@@ -8,6 +8,8 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
@@ -16,12 +18,17 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import LocalActivityIcon from '@mui/icons-material/LocalActivity';
-
+import imagen from "../imagenes/maluma.png";
 
 export default function Carta(props) {
- 
+  
+
+ const ruta = "/details/"+props.eventoId;
+ console.log(ruta)
+
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 300, maxHeight: 450 ,minHeight:450, marginTop :2}}>
       <CardHeader
      
         action={
@@ -30,13 +37,14 @@ export default function Carta(props) {
           </IconButton>
         }
         title={props.titulo}
+       
         subheader="September 14, 2016"
       />
       <CardMedia
         component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
+        height="200"
+        image={imagen}
+        alt="Imagen evento"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
@@ -53,6 +61,10 @@ export default function Carta(props) {
         <IconButton aria-label="settings">
             <LocalActivityIcon />
           </IconButton>
+        <Link href={ruta} variant="body2">
+        Ir a detalle
+        </Link>
+        
       </CardActions>
      
     </Card>
--- src/views/Details.js
+++ src/views/Details.js
@@ -1,21 +1,23 @@
 
 import React, {Component, useEffect, useState } from "react";
 import Menu from '../components/Menu';
+import CuerpoDetalles from '../components/CuerpoDetalles';
 import Cartas from "../components/Cartas";
 import '../styles/admin.sass';
 import { render } from "@testing-library/react";
 
-function Admin() {
+function Details() {
 
   return (
     <div>
-        <div className="cabecera"> <h1 className="pru">EVENTOS</h1>   </div>
+        <div className="cabecera"> <h1 className="pru">EVENTOS</h1></div>
     
     <Menu/>
-    <h1>HELLO DETAILS</h1>
+    <h1>DETALLES</h1>
+    <CuerpoDetalles/>
     
     </div>
   );}
 
 
-export default Admin;
+export default Details;
