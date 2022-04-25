45c0262249c242f69c816be52cee91e4a4e2d78b
DorDaniel1989 <danielovelleiro@outlook.es>
2022-04-25 12:57:04 +0200

    desincribir usuario desde perfil , comenzando css

--- src/components/InscripcionesUsuario.js
+++ src/components/InscripcionesUsuario.js
@@ -1,6 +1,11 @@
 
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import $ from 'jquery';
+import "jquery-ui-dist/jquery-ui";
+import "datatables.net-dt/js/dataTables.dataTables"
+import "datatables.net-dt/css/jquery.dataTables.min.css"
+import axios from 'axios';
 
 function InscripcionesUsuario() {
 
@@ -9,6 +14,7 @@ function InscripcionesUsuario() {
 
     useEffect(() => {
         obtenerDatos()
+
     }, [])
 
     const obtenerDatos = async () => {
@@ -18,25 +24,41 @@ function InscripcionesUsuario() {
         console.log(inscripciones)
     }
 
+    async function  EliminarInscripcion(Id){
 
+        const response = await axios.delete(` https://localhost:5001/api/Inscripcion/delete/${Id}`);
+        console.log(response)
+        obtenerDatos()
+    
+    }
 
     return (
-
 
         <div className="container">
 
             <h2>IDS DE EVENTOS INSCRIPCIONES DEL USUARIO</h2>
 
-            {
-                inscripciones.map(item => (
-
-                    <div  key={item.eventoId}>
-                    <p>{item.eventoId}</p>
-                    </div>
-
-                ))
-            }
-
+            <table className="tMisInscripciones" onLoad={$('.tMisInscripciones').DataTable()}>
+                <thead>
+                    <tr>
+                        <th>Seleccionar</th>
+                        <th>Inscripcion Id</th>
+                        <th>Evento Id</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        inscripciones.map(item => (
+                            <tr key={item.inscripcionId}>
+                                <td><button onClick={()=>EliminarInscripcion(item.inscripcionId)} className="btn btn-warning"> Desapuntar</button></td>
+                                <td>{item.inscripcionId}</td>
+                                <td>{item.eventoId}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+            <input type="button"  value="Seleccion"/>
         </div>
 
     )
