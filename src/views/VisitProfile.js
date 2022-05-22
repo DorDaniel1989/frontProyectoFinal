f6d2763366f0f2eb71ee72e6157373c816376982
DorDaniel1989 <danielovelleiro@outlook.es>
2022-05-22 18:26:58 +0200

    visit profile capada si es tu user

--- src/views/VisitProfile.js
+++ src/views/VisitProfile.js
@@ -3,11 +3,19 @@ import React from "react";
 import Menu from '../components/Menu';
 import FormularioLogin from '../components/FormularioLogin';
 import CuerpoVisitProfile from "../components/CuerpoVisitProfile"
+import { useParams } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { Navigate } from 'react-router-dom'
 
 
 function VisitProfile() {
 
+
+const { usuarioId } = useParams();
+const ruta ="/profile/"+usuarioId;
+console.log(ruta)
+
+  if ((localStorage.getItem('user') === null ) || JSON.parse(localStorage.getItem('user')).usuarioId != usuarioId) {
   return (
     <>
     <Menu/>
@@ -15,6 +23,10 @@ function VisitProfile() {
     <CuerpoVisitProfile/>
     <Footer/>
     </>
-  );}
+  )}else{
+    return (<Navigate to={ruta} />);
+  }
+
+}
 
 export default VisitProfile;
